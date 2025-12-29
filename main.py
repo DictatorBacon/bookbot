90e3bbd78a6ff7fa0fb59469d68bbae258e447e4
DictatorBacon <masonatanguay@gmail.com>
2025-12-28 19:42:07 -0800

    correct solution

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-def main():
-    get_book_text("books/frankenstein.txt")
 
 main()
