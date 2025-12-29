d6b76a44329c4c793c089446f314df81031b208b
DictatorBacon <masonatanguay@gmail.com>
2025-12-28 19:50:38 -0800

    added word counter

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
-
+    num_words()
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def num_words():
+    words = get_book_text("books/frankenstein.txt").split()
+    count = 0
+    for word in words:
+        count += 1
+    print(f"Found {count} total words")
+    return count
 
 main()
